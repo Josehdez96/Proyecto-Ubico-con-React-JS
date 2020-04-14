f67d09d2851e46a9c1d9e70030ca05777c147159
Jose Luis Alvarez Herrera <joseluh_1@hotmail.com>
2020-04-13 22:12:31 -0500

    PR example

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ubicoLogo from "../resources/companyImages/UbicoLogoLight.png"
+import SearchBar from "./Searchbar";
 
 const Hero = () => (
   <div className="container">
@@ -8,7 +9,12 @@ const Hero = () => (
         <img src={ubicoLogo} alt="Logo Ubico"/>
       </div>
     </div>
+    <div className="row text-center">
+        <div className="col">
+            <SearchBar />
+        </div>
+    </div>
   </div>
-)
+);
 
-export default Hero
+export default Hero;
