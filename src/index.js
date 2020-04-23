01af2352b8443c40091311f0ba735d2503e3197c
Jose Daniel Hernandez <josedanielhq@gmail.com>
2020-04-23 18:45:42 -0500

    Eliminación Thunk

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css"
 import App from "./containers/App.jsx";
 
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux" ;
-import reduxThunk from "redux-thunk"
+
 
 import reducers from "./reducers"
 
 const store = createStore( //Recibe 2 paramtetros
   reducers, //Todos los Reducers
-  {}, //Estado inicial
-  applyMiddleware(reduxThunk)
+  {} //Estado inicial
+
 )
 
 const container = document.getElementById('app')
